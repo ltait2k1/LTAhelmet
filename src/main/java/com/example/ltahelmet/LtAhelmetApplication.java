94e25a2d3c25b8f1f3981162b5c30d99ffd495d5
ltait2k1 <letuananh2k1.works@gmail.com>
2024-02-12 22:50:52 +0700

    first update

---
+++ src/main/java/com/example/ltahelmet/LtAhelmetApplication.java
@@ -0,0 +1,13 @@
+package com.example.ltahelmet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LtAhelmetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LtAhelmetApplication.class, args);
+    }
+
+}
